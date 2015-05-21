142ec9fd6f75ef01d4528fe1d71b523df66bb152
n0nr00t <@nonroot>
2015-05-21 02:06:05 -0500

    cacheMatrix
    with Example (system.time())

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Escribir una funcion para crear la cache de una matrix Inversa
+#
+# by @nonroot , 2015
+#
+# Que tambien funciona?, miremos los tiempos con y sin cache.
+# > x <- diag(5000)
+# > m <- makeCacheMatrix(x)
+# > system.time(cacheSolve(m))
+# user  system elapsed 
+# 62.182   0.704  64.535 
+# > system.time(cacheSolve(m))
+# user  system elapsed 
+# 0       0       0 
+# > system.time(cacheSolve(m))
+# user  system elapsed 
+# 0       0       0 
+# > 
+  
+#Crear una lista que puede almacenar la matrix "especial"
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInversa <- function(mInversa) m <<- mInversa
+  getInversa <- function() m
+  list(set = set, get = get,
+       setInversa = setInversa,
+       getInversa = getInversa)
 }
 
-
-## Write a short comment describing this function
-
+# Obtenga la inversa de la matrix, si la matrix de entrada esta nula, calculela
+# Esto solo se hace la primera vez, si no es nula, entonces obtenga la cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mInversa <- x$getInversa()
+  if(!is.null(mInversa)) {
+    return(mInversa)
+  }
+  matrix <- x$get()
+  mInversa <- solve(matrix)
+  x$setInversa(mInversa)
+  mInversa
 }
